a12a42d2ba749c031a753be0cda8d33f93790338
KIyA <yueyaye@163.com>
2020-11-02 17:29:41 +0800

    ElasticSearch 秀

--- src/main/java/com/clei/controller/ESTestController.java
+++ src/main/java/com/clei/controller/ESTestController.java
@@ -5,12 +5,14 @@ import com.clei.service.ESOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * elasticsearch操作测试 controller
@@ -48,10 +50,66 @@ public class ESTestController {
      *
      * @return
      */
+    @RequestMapping("/batchSave")
+    public String batchSave() {
+        List<Order> orderList = getOrderList2();
+
+        // esOrderService.saveAll(orderList);
+
+        orderList.forEach(o -> esOrderService.save(o));
+
+        return "success";
+    }
+
+    /**
+     * 添加/修改
+     */
+    @RequestMapping("/save")
+    public String save(@RequestBody Order order) {
+        esOrderService.save(order);
+
+        return "success";
+    }
+
+    /**
+     * 初始化
+     *
+     * @return
+     */
     @RequestMapping("/getAll")
     public List<Order> getAll() {
         List<Order> list = esOrderService.getAll();
         return list;
+    }
+
+    /**
+     * 简单分组
+     *
+     * @return
+     */
+    @RequestMapping("/group")
+    public Map<String, Object> group() {
+        return esOrderService.simpleGroup();
+    }
+
+    /**
+     * 简单分组
+     *
+     * @return
+     */
+    @RequestMapping("/complexGroup")
+    public List<Map<String, Object>> complexGroup() {
+        return esOrderService.complexGroup();
+    }
+
+    /**
+     * 应付最大值
+     *
+     * @return
+     */
+    @RequestMapping("/max")
+    public double max() {
+        return esOrderService.max();
     }
 
     /**
@@ -113,6 +171,74 @@ public class ESTestController {
         o3.setCreateTime(System.currentTimeMillis() - 1111 * 333);
         o3.setPayTime(System.currentTimeMillis());
         o3.setRemark("十步杀一人");
+
+        List<Order> list = new ArrayList<>(3);
+        list.add(o1);
+        list.add(o2);
+        list.add(o3);
+
+        return list;
+    }
+
+    /**
+     * 创建一些订单
+     *
+     * @return
+     */
+    private List<Order> getOrderList2() {
+        Order o1 = new Order();
+        o1.setId(4L);
+        o1.setOrderName("orderName1");
+        o1.setProductId(11L);
+        o1.setProductName("蓝色玻璃杯");
+        o1.setSellerId(22L);
+        o1.setSellerName("蓝色玻璃杯商店");
+        o1.setBuyerId(789L);
+        o1.setBuyerName("饿虎");
+        o1.setBuyNum(12);
+        o1.setUnitPrice(3D);
+        o1.setTotalPrice(o1.getBuyNum() * o1.getUnitPrice());
+        o1.setDeductionPrice(12D);
+        o1.setPayPrice(new BigDecimal(o1.getTotalPrice()).subtract(new BigDecimal(o1.getDeductionPrice())).doubleValue());
+        o1.setCreateTime(System.currentTimeMillis() - 333 * 444);
+        o1.setPayTime(System.currentTimeMillis());
+        o1.setRemark("好饿好饿好饿");
+
+        Order o2 = new Order();
+        o2.setId(5L);
+        o2.setOrderName("orderName2");
+        o2.setProductId(123L);
+        o2.setProductName("康师傅老坛酸菜面");
+        o2.setSellerId(456L);
+        o2.setSellerName("康师傅专卖店");
+        o2.setBuyerId(5566L);
+        o2.setBuyerName("刀客");
+        o2.setBuyNum(24);
+        o2.setUnitPrice(3.5D);
+        o2.setTotalPrice(o2.getBuyNum() * o2.getUnitPrice());
+        o2.setDeductionPrice(18D);
+        o2.setPayPrice(new BigDecimal(o2.getTotalPrice()).subtract(new BigDecimal(o2.getDeductionPrice())).doubleValue());
+        o2.setCreateTime(System.currentTimeMillis() - 123 * 456);
+        o2.setPayTime(System.currentTimeMillis());
+        o2.setRemark("研究一下");
+
+        Order o3 = new Order();
+        o3.setId(6L);
+        o3.setOrderName("orderName3");
+        o3.setProductId(1122L);
+        o3.setProductName("未开刃的长剑");
+        o3.setSellerId(3344L);
+        o3.setSellerName("血腥武器店");
+        o3.setBuyerId(33L);
+        o3.setBuyerName("老贼");
+        o3.setBuyNum(2);
+        o3.setUnitPrice(4399D);
+        o3.setTotalPrice(o3.getBuyNum() * o3.getUnitPrice());
+        o3.setDeductionPrice(998D);
+        o3.setPayPrice(new BigDecimal(o3.getTotalPrice()).subtract(new BigDecimal(o3.getDeductionPrice())).doubleValue());
+        o3.setCreateTime(System.currentTimeMillis() - 1111 * 333);
+        o3.setPayTime(System.currentTimeMillis());
+        o3.setRemark("十年磨一剑");
 
         List<Order> list = new ArrayList<>(3);
         list.add(o1);
--- src/main/java/com/clei/service/ESOrderService.java
+++ src/main/java/com/clei/service/ESOrderService.java
@@ -2,21 +2,40 @@ package com.clei.service;
 
 import com.clei.entity.elasticsearch.Order;
 import com.clei.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.ParsedStringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.metrics.max.ParsedMax;
+import org.elasticsearch.search.aggregations.metrics.sum.ParsedSum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilterBuilder;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ESOrderService implements ESService {
 
-    @Autowired
     private ElasticsearchRestTemplate template;
 
-    @Autowired
     private OrderRepository orderRepository;
+
+    // 构造器注入可省略@Autowired注解
+    // @Autowired
+    public ESOrderService(ElasticsearchRestTemplate template, OrderRepository orderRepository) {
+        this.template = template;
+        this.orderRepository = orderRepository;
+    }
 
     /**
      * 创建索引
@@ -26,6 +45,15 @@ public class ESOrderService implements ESService {
     @Override
     public boolean createIndex() {
         return template.createIndex(Order.class);
+    }
+
+    /**
+     * 单个插入/修改
+     *
+     * @param order
+     */
+    public void save(Order order) {
+        orderRepository.save(order);
     }
 
     /**
@@ -47,5 +75,148 @@ public class ESOrderService implements ESService {
         List<Order> list = new ArrayList<>();
         iterable.forEach(i -> list.add(i));
         return list;
+    }
+
+    /**
+     * 简单分组
+     *
+     * @return
+     */
+    public Map<String, Object> simpleGroup() {
+        NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder()
+                .addAggregation(AggregationBuilders.terms("productName_group").field("productName"))
+                .withPageable(PageRequest.of(0, 1))
+                .build();
+
+        Page<Order> result = orderRepository.search(nativeSearchQuery);
+
+        result.get().forEach(System.out::println);
+
+        System.out.println(result.getClass().getName());
+
+        AggregatedPage<Order> aggregatedPage = (AggregatedPage<Order>) result;
+
+        Aggregation aggregation = aggregatedPage.getAggregation("productName_group");
+
+        System.out.println(aggregation.getClass().getName());
+
+        ParsedStringTerms terms = (ParsedStringTerms) aggregation;
+
+        List<? extends Terms.Bucket> buckets = terms.getBuckets();
+
+        System.out.println("name : " + terms.getName());
+        System.out.println("type : " + terms.getType());
+        buckets.forEach(b -> {
+            System.out.println(b.getKeyAsString() + " : " + b.getDocCount());
+        });
+
+        return terms.getMetaData();
+    }
+
+    /**
+     * 简单max
+     *
+     * @return
+     */
+    public Double max() {
+        NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder()
+                // 啥字段也不查 减少数据量
+                .withSourceFilter(new FetchSourceFilterBuilder().withIncludes("").build())
+                .addAggregation(AggregationBuilders.max("payPrice_max").field("payPrice"))
+                .withPageable(PageRequest.of(0, 1))
+                .build();
+
+        Page<Order> result = orderRepository.search(nativeSearchQuery);
+
+        result.get().forEach(System.out::println);
+
+        System.out.println(result.getClass().getName());
+
+        AggregatedPage<Order> aggregatedPage = (AggregatedPage<Order>) result;
+
+        Aggregation aggregation = aggregatedPage.getAggregation("payPrice_max");
+
+        System.out.println(aggregation.getClass().getName());
+
+        ParsedMax max = (ParsedMax) aggregation;
+
+        double payPriceMax = max.getValue();
+
+        return payPriceMax;
+    }
+
+    /**
+     * 复杂分组
+     * 找到各个店家下面销量最高的产品及其总收入金额
+     *
+     * @return
+     */
+    public List<Map<String, Object>> complexGroup() {
+        NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder()
+                .addAggregation(AggregationBuilders.terms("sellerName_group").field("sellerName")
+                        .subAggregation(AggregationBuilders.terms("productName_group").field("productName")
+                                .subAggregation(AggregationBuilders.sum("payPrice_sum").field("payPrice"))))
+                .withPageable(PageRequest.of(0, 1))
+                .build();
+
+        Page<Order> result = orderRepository.search(nativeSearchQuery);
+
+        result.get().forEach(System.out::println);
+
+        System.out.println(result.getClass().getName());
+
+        AggregatedPage<Order> aggregatedPage = (AggregatedPage<Order>) result;
+
+        Aggregation aggregation = aggregatedPage.getAggregation("sellerName_group");
+
+        System.out.println(aggregation.getClass().getName());
+
+        ParsedStringTerms terms = (ParsedStringTerms) aggregation;
+
+        List<? extends Terms.Bucket> buckets = terms.getBuckets();
+
+        System.out.println("name : " + terms.getName());
+        System.out.println("type : " + terms.getType());
+
+        // 结果集
+        List<Map<String, Object>> resultList = new ArrayList<>(3);
+
+        buckets.forEach(b -> {
+            Aggregations aggregations = b.getAggregations();
+            System.out.println(aggregations.getClass().getName());
+            aggregations.forEach(a -> {
+                System.out.println(a.getClass().getName());
+                ParsedStringTerms parsedStringTerms = (ParsedStringTerms) a;
+                List<? extends Terms.Bucket> buckets1 = parsedStringTerms.getBuckets();
+
+                double max = 0;
+                // 找到最大的
+                for (Terms.Bucket bb : buckets1) {
+                    Aggregation agg = bb.getAggregations().get("payPrice_sum");
+                    ParsedSum sum = (ParsedSum) agg;
+                    double s = sum.getValue();
+                    max = max > s ? max : s;
+                }
+                final double resultMax = max;
+                // 匹配谁是最大的
+                for (Terms.Bucket bb : buckets1) {
+                    Aggregation agg = bb.getAggregations().get("payPrice_sum");
+                    System.out.println(agg.getClass().getName());
+
+                    ParsedSum sum = (ParsedSum) agg;
+                    double s = sum.getValue();
+                    if (s == resultMax) {
+                        Map<String, Object> m = new HashMap<>();
+                        m.put("sellerName", b.getKey());
+                        m.put("productName", bb.getKey());
+                        m.put("payPriceSumMax", s);
+                        resultList.add(m);
+                    }
+                }
+
+            });
+        });
+
+        return resultList;
     }
 }
